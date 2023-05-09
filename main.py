c1730b150bf62b6b4800494a72e60c2091475a77
Tanguy POINGT <poingt@et.esiea.fr>
2023-05-09 19:47:04 +0200

    pas loin de la fin :)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def count_images_in_folder(folder_path):
@@ -32,8 +33,8 @@ class iris_detection():
             i += 1
 
 
-    def ShowImage(self):
-        cv2.imshow("StartPicture", self.StartPicture[0])
+    def ShowImage(self, indice):
+        cv2.imshow("StartPicture", self.StartPicture[indice])
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     def better_picture(self):
@@ -46,12 +47,38 @@ class iris_detection():
         for i in range(len(self.StartPicture)):
             self.StartPicture[i] = cv2.Canny(self.StartPicture[i], 100, 200)
 
+    def find_pattern(self, img_source, img_template):
+        contour1, _ = cv2.findContours(self.StartPicture[img_source].copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contour2, _ = cv2.findContours(self.StartPicture[img_template].copy(), cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_SIMPLE)
+        matches = []
+        for ctn1 in contour1:
+            for ctn2 in contour2:
+                match = cv2.matchShapes(ctn1, ctn2, cv2.CONTOURS_MATCH_I2, 0)
+                if match < 0.1:
+                    matches.append((ctn1, ctn2))
+
+        img_fini1 = self.StartPicture[img_source].copy()
+        img_fini2 = self.StartPicture[img_template].copy()
+
+        for mt in matches:
+            contour1, contour2 = mt
+            x, y, w, h = cv2.boundingRect(contour1)
+            img_fini1 = cv2.rectangle(img_fini1, (x, y), (x + w, y + h), (255, 255, 255), 2)
+            x, y, w, h = cv2.boundingRect(contour2)
+            img_fini2 = cv2.rectangle(img_fini2, (x, y), (x + w, y + h), (255, 255, 255), 2)
+
+        cv2.imshow("Image 1", img_fini1)
+        cv2.imshow("Image 2", img_fini2)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
 image_count = count_images_in_folder("Intput_database")
-iris_dec = iris_detection("Intput_database", image_count);
+iris_dec = iris_detection("Intput_database", image_count)
 iris_dec.load_image_from_folder()
-iris_dec.ShowImage()
-iris_dec.ShowImage()
 iris_dec.better_picture()
-iris_dec.ShowImage()
 iris_dec.iris_detection()
-iris_dec.ShowImage()
+user_img_source = int(input("Enter the number of the source picture: "))
+user_img_template = int(input("Enter the number of the template picture: "))
+iris_dec.find_pattern(user_img_source, user_img_template)
